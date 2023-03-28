b2835bac71f49a1c98c0d9cfbc00ea8cdaa96f63
Felpe Pichinual <fpichinual@gmail.com>
2023-03-28 02:09:17 -0700

    Finish the JS gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,35 @@
 function activateGallery(){
-    alert("Hello from gallery file!");
+
+
+    let thumbnails = document
+        .querySelectorAll("#gallery-thumbs > div > img");
+
+    let mainImage = document.querySelector("#gallery-photo > img");
+
+    thumbnails.forEach(function (thumbnail) {
+        thumbnail.addEventListener("click", function () {
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            let newAlt = thumbnail.alt;
+            mainImage.setAttribute("src", newImageSrc);
+            mainImage.setAttribute("alt", newAlt);
+
+            // Image info to be updated
+            let galleryInfo = document.querySelector("#gallery-info");
+            let title
+                = galleryInfo.querySelector(".title");
+            let description = galleryInfo.querySelector(".description");
+
+            // change which image is current.
+
+            document.querySelector(".current").classList.remove("current");
+            thumbnail.parentNode.classList.add("current");
+
+            // Update image info.
+            title.innerHTML = thumbnail.dataset.title;
+
+            description.innerHTML = thumbnail.dataset.description;
+
+        });
+    });
 }
+
